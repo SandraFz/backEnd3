986cd0f7c6a289329ae5a9f765e04da352b6b88c
Sandra <sandranfernandez@gmail.com>
2022-04-27 10:52:16 -0300

    Prueba primer commit

--- src/main/java/com/portfolio/MyPortfolio8/controller/Controller.java
+++ src/main/java/com/portfolio/MyPortfolio8/controller/Controller.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,12 +25,12 @@ public class Controller {
     @GetMapping ("/hola/{name}")
     public String holaMundo(@PathVariable String name) {
         return "Hola, " + name;
-    }
+    }*/
     
     @GetMapping ("/chau")
     public String chauYou (@RequestParam String name){
         return "Chau, " +name;
-    }*/
+    }
     
     @PostMapping("/createPerson")
     public void createPerson(@RequestBody Person pers){
