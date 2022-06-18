0afb291c0e67409df2540e2fa0195088bcfc8ccd
Sandra <sandranfernandez@gmail.com>
2022-06-18 20:39:43 -0300

    Idem

--- src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
+++ src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
@@ -1,15 +1,11 @@
 package com.portfolio.MyPortfolio8;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyPortfolio8Application {
@@ -18,15 +14,13 @@ public class MyPortfolio8Application {
 		SpringApplication.run(MyPortfolio8Application.class, args);
 	}
 
-@Bean
-public CorsFilter corsFilter() {
-final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-final CorsConfiguration config = new CorsConfiguration();
-config.setAllowCredentials(true);
-config.setAllowedOrigins(Collections.singletonList("*"));
-config.setAllowedHeaders(Collections.singletonList("*"));
-config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
-source.registerCorsConfiguration("/**", config);
-return new CorsFilter(source);
+        @Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+registry.addMapping("/**");
+}
 }
 }
