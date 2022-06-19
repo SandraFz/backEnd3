331346878c5f009e4bfc4a7f7fb37be089740e65
Sandra <sandranfernandez@gmail.com>
2022-06-18 21:27:14 -0300

    NN

--- src/main/java/com/portfolio/MyPortfolio8/controller/personController.java
+++ src/main/java/com/portfolio/MyPortfolio8/controller/personController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/person")
 public class personController {
