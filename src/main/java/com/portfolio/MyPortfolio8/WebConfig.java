2a13e8e49d51f87e97d58f6c192a2b998dc71b78
Sandra <sandranfernandez@gmail.com>
2022-06-21 20:06:32 -0300

    AddImg/modificación de WebConfig.

--- src/main/java/com/portfolio/MyPortfolio8/WebConfig.java
+++ src/main/java/com/portfolio/MyPortfolio8/WebConfig.java
@@ -1,6 +1,14 @@
 package com.portfolio.MyPortfolio8;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,4 +21,17 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**");
     }
+    
+    @Bean
+    public CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+    }
 }
+
