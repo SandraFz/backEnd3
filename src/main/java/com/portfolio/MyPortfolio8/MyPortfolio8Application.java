d322c0052748710c064963a004297646d82ded7d
Sandra <sandranfernandez@gmail.com>
2022-06-23 19:40:02 -0300

    cors1/agregué el Bean recomendado por el profesor en el foro.

--- src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
+++ src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
@@ -1,8 +1,13 @@
 package com.portfolio.MyPortfolio8;
 
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class MyPortfolio8Application {
@@ -11,6 +16,21 @@ public class MyPortfolio8Application {
 		SpringApplication.run(MyPortfolio8Application.class, args);
 	}
         
+@Bean
+public CorsFilter corsFilter() {
+CorsConfiguration corsConfiguration = new CorsConfiguration();
+corsConfiguration.setAllowCredentials(true);
+corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-argentina-prog-f5593.web.app/"));
+corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+"Accept", "Authorization", "Origin, Accept", "X-Request-With",
+"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-type", "Accept", "Authorization",
+"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+corsConfiguration.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS"));
+UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+return new CorsFilter(urlBasedCorsConfigurationSource);
+}
 
 
 }
