c7e89949e4b5f56e0db712eac6ca62aedea367e8
Sandra <sandranfernandez@gmail.com>
2022-06-24 18:59:38 -0300

    cors1/corrección de url en configuración CORS.

--- src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
+++ src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
@@ -30,7 +30,7 @@ public class MyPortfolio8Application {
 public CorsFilter corsFilter() {
 CorsConfiguration corsConfiguration = new CorsConfiguration();
 corsConfiguration.setAllowCredentials(true);
-corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
+corsConfiguration.setAllowedOrigins(Arrays.asList("https://afternoon-plains-82162.herokuapp.com"));
 corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 "Accept", "Authorization", "Origin, Accept", "X-Request-With",
 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
