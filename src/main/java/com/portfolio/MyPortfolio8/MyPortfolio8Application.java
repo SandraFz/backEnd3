6fabc41c82f62cd3db57ba601e189f1537f5ddf5
Sandra <sandranfernandez@gmail.com>
2022-06-24 21:08:42 -0300

    cors1/prueba con solo API local.

--- src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
+++ src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
@@ -25,12 +25,12 @@ public class MyPortfolio8Application {
 		};
 	}        */
 
-/*
+
 @Bean
 public CorsFilter corsFilter() {
 CorsConfiguration corsConfiguration = new CorsConfiguration();
 corsConfiguration.setAllowCredentials(true);
-corsConfiguration.setAllowedOrigins(Arrays.asList("hhttps://portfolio-argentina-prog-f5593.web.app"));
+corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-argentina-prog-f5593.web.app"));//http://localhost:4200
 corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 "Accept", "Authorization", "Origin, Accept", "X-Request-With",
 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
@@ -41,6 +41,6 @@ UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCo
 urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 return new CorsFilter(urlBasedCorsConfigurationSource);
 }
-*/
+
 
 }
--- src/main/java/com/portfolio/MyPortfolio8/controller/personController.java
+++ src/main/java/com/portfolio/MyPortfolio8/controller/personController.java
@@ -18,8 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@CrossOrigin(origins="*")
+@RestController 
 @RequestMapping("/person")
 public class personController {
     
