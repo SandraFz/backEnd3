8421e39373deffa46d71b0b710cc3097c656aba9
Sandra <sandranfernandez@gmail.com>
2022-06-27 20:23:48 -0300

    Bean

--- src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
+++ src/main/java/com/portfolio/MyPortfolio8/MyPortfolio8Application.java
@@ -15,22 +15,12 @@ public class MyPortfolio8Application {
 	public static void main(String[] args) {
 		SpringApplication.run(MyPortfolio8Application.class, args);
 	}
-/*
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080");
-			}
-		};
-	}        */
-
 
 @Bean
 public CorsFilter corsFilter() {
 CorsConfiguration corsConfiguration = new CorsConfiguration();
 corsConfiguration.setAllowCredentials(true);
-corsConfiguration.setAllowedOrigins(Arrays.asList("https://portfolio-argentina-prog-f5593.web.app"));//http://localhost:4200
+corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));//
 corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 "Accept", "Authorization", "Origin, Accept", "X-Request-With",
 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
